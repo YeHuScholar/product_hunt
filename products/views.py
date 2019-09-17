a4a0ef5dd58d153e6049921e85ff4afed78d5217
XYRecluse <lixy2017@aliyun.com>
2019-09-18 07:28:20 +0800

    更新了产品发布功能

--- products/views.py
+++ products/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from 
 
 # Create your views here.
+
 
 def product_list(request):
     return render(request, 'product_list.html')
 
-
+@login_required()
 def publish(request):
     if request.method == 'GET':
         return render(request, "publish.html")
     elif request.method == 'POST':
-        app_name = request.POST['APP名称']
+        title = request.POST['标题']
         intro = request.POST['介绍']
         url = request.POST['APP链接']
         icon = request.FILES['APP图标']
         image = request.FILES['大图']
+
+        product = Product()
         return render(request, 'publish.html')
