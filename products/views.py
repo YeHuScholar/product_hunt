79125df1e36055876232f8f37f4609137f983c6b
XYRecluse <lixy2017@aliyun.com>
2019-09-18 17:11:40 +0800

    完成产品发布功能

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from 
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 
@@ -20,4 +21,14 @@ def publish(request):
         image = request.FILES['大图']
 
         product = Product()
-        return render(request, 'publish.html')
+        product.title = title
+        product.intro = intro
+        product.url = url
+        product.icon = icon
+        product.image = image
+
+        product.pub_date = timezone.datetime.now()
+        product.hunter = request.user
+
+        product.save()
+        return redirect('主页')
